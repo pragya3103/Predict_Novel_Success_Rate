7e1453904b98bb0ca0badbc84c1be86d8355dd7d
pragya3103 <pragya3103@gmail.com>
2018-08-02 23:04:33 +0530

    Add files via upload

---
+++ averagewords.R
@@ -0,0 +1,11 @@
+
+Average_word_per_sen = function(novel){
+n = length(novel)
+list_novel = unlist(novel)
+list_novel = list_novel[1:n]
+sent_per_doc = tokenizers::count_sentences(list_novel)
+total_words = tokenizers::count_words(list_novel)
+
+Average = total_words/sent_per_doc
+return(Average)
+}
